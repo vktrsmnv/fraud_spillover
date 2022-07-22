082026f80714377afa557cad33d25dfda34e2f72
Viktoriia Semenova <vktrsmnv@qsmc220.sowi.uni-mannheim.de>
2022-07-22 23:17:21 +0200

    one more plot

--- code/04_plot_diffs.R
+++ code/04_plot_diffs.R
@@ -343,6 +343,187 @@ ggsave(plt,
        width = 10
 )
 
+### Political: Control Condition Only ####
+pp <-
+  prep_plotting("output/ol_main_pol.rds",
+                output = "diffs",
+                model = "condition")
+
+arrows <- data.frame(
+  x1 = 0.19,
+  x2 = 0.1,
+  y1 = 1.55,
+  y2 = 1.2,
+  Condition = c("Control"),
+  institution_facet_name = levels(pp$institution_facet_name)[1]
+) %>%
+  mutate(
+    institution_facet_name = as_factor(institution_facet_name) %>%
+      fct_expand(
+        levels(temp$institution_facet_name)
+      )
+  )
+lst <- list()
+lst[[1]] <- pp %>%
+  filter(condition == "Control",
+         institution %in% levels(pp$institution)[1:4]) %>%
+  ggplot(aes(
+    x = median,
+    y = category
+  )) +
+  geom_pointrange(
+    aes(
+      x = median,
+      xmin = lower,
+      xmax = upper,
+      y = category,
+      color = condition,
+      shape = condition,
+      alpha = significant
+    ),
+    position = position_dodge(1),
+    size = 0.4
+  ) +
+  facet_wrap(
+    . ~ institution_facet_name
+    # cols = vars(institution_facet_name),
+    # rows = vars(condition),
+    ncol = 4
+  ) +
+  scale_color_viridis(
+    discrete = T,
+    option = "C",
+    end = 0.8
+  ) +
+  scale_shape_manual(values = c(16, 15, 17, 8)) +
+  scale_alpha_manual(values = c(0.4, 1)) +
+  labs(
+    y = "Confidence",
+    x = paste0(
+      "Probability(Category|Fraud) - Probability(Category|Control)"
+    ),
+    shape = "",
+    color = "",
+    title = ""
+  ) +
+  geom_vline(
+    xintercept = 0,
+    alpha = 0.5
+  ) +
+  guides(color = "none", alpha = "none",
+         shape = "none") +
+  geom_curve(
+    data = arrows,
+    aes(
+      x = x1,
+      y = y1,
+      xend = x2,
+      yend = y2
+    ),
+    arrow = arrow(length = unit(0.08, "inch")),
+    size = 0.5,
+    alpha = 0.3,
+    color = plasma(1),
+    curvature = -0.3
+  ) +
+  geom_abline(
+    intercept = 2.5,
+    slope = -15,
+    size = 0.5,
+    alpha = 0.3,
+    linetype = 2,
+    color = plasma(1)
+  ) +
+  geom_text(
+    data = data.frame(
+      median = 0.2,
+      category = "Not very\nmuch",
+      condition = c("Control"),
+      institution_facet_name = levels(pp$institution_facet_name)[1]
+    ) %>%
+      mutate(
+        institution_facet_name = as_factor(institution_facet_name) %>%
+          fct_expand(
+            levels(pp$institution_facet_name)
+          ),
+        category = as.factor(category) %>%
+          fct_expand(levels(pp$category))
+      ),
+    label = "fraud\ndecreases\ntrust",
+    size = 2.7,
+    nudge_y = 0.3,
+    color = plasma(1, 0.7)
+  ) +
+  xlim(-0.35, 0.35)
+
+
+lst[[2]] <- pp %>%
+  filter(condition == "Control",
+         institution %in% levels(pp$institution)[5:8]) %>%
+  ggplot(aes(
+    x = median,
+    y = category
+  )) +
+  geom_pointrange(
+    aes(
+      x = median,
+      xmin = lower,
+      xmax = upper,
+      y = category,
+      color = condition,
+      shape = condition,
+      alpha = significant
+    ),
+    position = position_dodge(1),
+    size = 0.4
+  ) +
+  facet_grid(
+    cols = vars(institution_facet_name),
+    # rows = vars(condition),
+    # ncol = 4
+  ) +
+  scale_color_viridis(
+    discrete = T,
+    option = "C",
+    end = 0.8
+  ) +
+  scale_shape_manual(values = c(16, 15, 17, 8)) +
+  scale_alpha_manual(values = c(0.4, 1)) +
+  labs(
+    y = "Confidence",
+    x = paste0(
+      "Probability(Category|Fraud) - Probability(Category|Control)"
+    ),
+    shape = "",
+    color = "",
+    title = ""
+  ) +
+  geom_vline(
+    xintercept = 0,
+    alpha = 0.5
+  ) +
+  guides(color = "none", alpha = "none",
+         shape = "none") +
+  geom_abline(
+    intercept = 2.5,
+    slope = -15,
+    size = 0.5,
+    alpha = 0.3,
+    linetype = 2,
+    color = plasma(1)
+  ) +
+  xlim(-0.35, 0.35)
+
+
+plt <- lst[[1]] / lst[[2]] +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "bottom")
+ggsave(plt,
+       filename = paste0("figs/diffs_pooled_control.png"),
+       height = 6,
+       width = 10
+)
+
 ### Non-political ####
 pp <-
   prep_plotting("output/ol_main_npol.rds",
